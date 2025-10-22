01afcbff041c3915d7f61549fe807cf89ba95d79
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-10-22 16:41:42 +0000

    [Modificaciones y ajustes en el footer] index.html: se ingreso el link a font awesome actualizado footer.jsx: se hizo el footer desde cero

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,25 @@
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<div className="container-fluid justify-content-center p-0">
+		<footer className="footer mt-auto py-3 text-center  px-5 mx-5 mt-5 mb-2 border-top">
+			<div className="row">
+				<div className="col-3">
+					<p className="d-flex mb-0 justify-content center"><strong><i className="fa-solid fa-envelope"></i> Email</strong></p>
+					<a className="d-flex justify-content start" target="blank" href="mailto: julian.ramirez2507@gmail.com">julian.ramirez2507@gmail.com</a>
+				</div>
+				<div className="col-3">
+					<p className="d-flex mb-0 justify-content center"><strong><i className="fa-solid fa-envelope"></i> Github</strong></p>
+					<a className="d-flex justify-content start" target="blank" href="https://github.com/jramirez0982">julian.ramirez2507@gmail.com</a>
+				</div>
+				<div className="col-3">
+					<p className="d-flex mb-0 justify-content center"><strong><i className="fa-solid fa-envelope"></i> Teléfono</strong></p>
+					<a className="d-flex justify-content start" target="blank">+57 3113770557</a>
+				</div>
+				<div className="col-3">
+					<p className="d-flex mb-0 justify-content center"><strong><i className="fa-solid fa-envelope"></i> Linkedin</strong></p>
+					<a className="d-flex justify-content start" target="blank" href="https://www.linkedin.com/in/julian-andres-ramirez-ramirez/">/in/julian-andres-ramirez-ramirez/</a>
+				</div>
+
+			</div>
+		</footer>
+	</div>
 );
