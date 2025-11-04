011f7630fc43039f49f650371f718acfbea564bf
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-11-04 17:16:44 +0000

    [APLICAR CLASES PARA POSICIONAR TEXTO Y FOTO] Home.jsx: se aplicaron clases al texto y la imagen para que queden uno al lado del otro y para que en dispositivos moviles el texto se traslade hacia abajo

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -35,11 +35,27 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5 mb-5">
-			
-			<p className="">
-				<img src={julianRamirez} className="img-fluid mb-3 rounded-circle" style={{ width: "20%" }}alt="Rigo Baby" />
-			</p>
-			
+			{/* Contenedor principal para centrar la fila horizontalmente */}
+			<div className="d-flex justify-content-center">
+				{/* Contenedor de la imagen y el texto: usamos d-flex para que estén lado a lado */}
+				{/* Usamos 'align-items-center' para alinearlos verticalmente al centro (opcional) */}
+				{/* Se usa 'col-lg-8' o un tamaño similar para limitar el ancho del contenido y mantener el centrado */}
+				<div className="d-flex flex-column flex-lg-row align-items-center col-10 col-md-8 col-lg-6">
+					{/* Contenedor de la Imagen */}
+					<div className="flex-shrink-0 me-4"> {/* flex-shrink-0 previene que se encoja. me-4 es margin-right */}
+						<img
+							src={julianRamirez}
+							className="img-fluid rounded-circle"
+							style={{ width: "200px", height: "200px", objectFit: "cover" }} /* Tamaño fijo para la imagen */
+							alt="Julian Ramirez"
+						/>
+					</div>
+					{/* Contenedor del Texto (Lorem) */}
+					<div className="flex-grow-1 text-start"> {/* flex-grow-1 hace que ocupe el espacio restante. text-start para que el texto justifique a la izquierda dentro de este div */}
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, tenetur, consequatur deleniti tempore aliquam dolores esse magnam doloribus neque officiis rem dolor officia eaque nostrum vero. Cumque voluptates incidunt delectus repellat reiciendis deserunt minima sapiente expedita. Quia sequi est nesciunt, fugit rem ex iste nulla, iusto soluta labore eveniet! Sapiente?
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }; 
