3f6194baa12cfd95dcc33d3d381d3dad64543e9b
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-12-17 17:23:20 +0000

    [SE ESCRIBE TODA LA SECCION DE EXPERIENCIA]
    Home.jsx: Se escribio todo lo relacionado con experiencia laboral, dando alineacion y margenes en la linea de tiempo

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -181,180 +181,225 @@ export const Home = () => {
 
 	return (
 		<div>
-		<Navbar />
-		<div className="text-center mt-5 pt-2">
-			
-			{/* Contenedor principal para centrar la fila horizontalmente */}
-			<div className="d-flex justify-content-center border border-3 rounded-5 mx-5 border-custom-light-blue">
-				{/* Contenedor de la imagen y el texto: usamos d-flex para que estén lado a lado */}
-				{/* Usamos 'align-items-center' para alinearlos verticalmente al centro (opcional) */}
-				{/* Se usa 'col-lg-8' o un tamaño similar para limitar el ancho del contenido y mantener el centrado */}
-				<div className="d-flex flex-column flex-lg-row align-items-center col-12 col-md-10 col-lg-8 p-4">
-					{/* Contenedor de la Imagen */}
-					<div className="flex-shrink-0 me-4"> {/* flex-shrink-0 previene que se encoja. me-4 es margin-right */}
-						<img
-							src={julianRamirez}
-							className="img-fluid rounded-circle"
-							style={{ width: "250px", height: "250px", objectFit: "cover" }} /* Tamaño fijo para la imagen */
-							alt="Julian Ramirez"
-						/>
-					</div>
+			<Navbar />
+			<div className="text-center mt-5 pt-2">
 
-					<div>
-						<div id="perfil" className="fs-4 text-start fw-bold fst-italic">
-							Perfil
+				{/* Contenedor principal para centrar la fila horizontalmente */}
+				<div className="d-flex justify-content-center border border-3 rounded-5 mx-5 border-custom-light-blue">
+					{/* Contenedor de la imagen y el texto: usamos d-flex para que estén lado a lado */}
+					{/* Usamos 'align-items-center' para alinearlos verticalmente al centro (opcional) */}
+					{/* Se usa 'col-lg-8' o un tamaño similar para limitar el ancho del contenido y mantener el centrado */}
+					<div className="d-flex flex-column flex-lg-row align-items-center col-12 col-md-10 col-lg-8 p-4">
+						{/* Contenedor de la Imagen */}
+						<div className="flex-shrink-0 me-4"> {/* flex-shrink-0 previene que se encoja. me-4 es margin-right */}
+							<img
+								src={julianRamirez}
+								className="img-fluid rounded-circle"
+								style={{ width: "250px", height: "250px", objectFit: "cover" }} /* Tamaño fijo para la imagen */
+								alt="Julian Ramirez"
+							/>
 						</div>
-						{/* Contenedor del Texto (Lorem) */}
-						<div className="fs-6 font-base lead flex-grow-1 text-justify mt-2 text-start"> <strong> {/* flex-grow-1 hace que ocupe el espacio restante. text-start para que el texto justifique a la izquierda dentro de este div */}
-							Ingeniero Electrónico y Magíster en Ciencias Computacionales, poseo una sólida trayectoria
-							profesional liderando equipos y gestionando proyectos tecnológicos. Soy Full Stack Developer
-							experto en Python, JavaScript, React, Flask y SQL. Mi experiencia en sectores como el aeronáutico y
-							el mantenimiento industrial me permite estructurar, optimizar y liderar proyectos creando soluciones escalables y seguras.
-							Mi enfoque es aportar valor, conectando las necesidades de negocio con la tecnología a través de arquitecturas eficientes,
-							conectando la visión de negocio con la excelencia técnica. </strong>
+
+						<div>
+							<div id="perfil" className="fs-4 text-start fw-bold fst-italic">
+								Perfil
+							</div>
+							{/* Contenedor del Texto (Lorem) */}
+							<div className="fs-6 font-base lead flex-grow-1 text-justify mt-2 text-start"> <strong> {/* flex-grow-1 hace que ocupe el espacio restante. text-start para que el texto justifique a la izquierda dentro de este div */}
+								Ingeniero Electrónico y Magíster en Ciencias Computacionales, poseo una sólida trayectoria
+								profesional liderando equipos y gestionando proyectos tecnológicos. Soy Full Stack Developer
+								experto en Python, JavaScript, React, Flask y SQL. Mi experiencia en sectores como el aeronáutico y
+								el mantenimiento industrial me permite estructurar, optimizar y liderar proyectos creando soluciones escalables y seguras.
+								Mi enfoque es aportar valor, conectando las necesidades de negocio con la tecnología a través de arquitecturas eficientes,
+								conectando la visión de negocio con la excelencia técnica. </strong>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div id="educacion" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
-				<div className="fs-3 fw-bold mb-3">
-					Formación Académica
-				</div>
-
-				<div className="d-flex">
-					<div className="row g-0 justify-content-center">
-						{
-							estudiosFormales.map((estudios, index) => {
-								return (
-									<div className="col-md-5 m-1 p-2">
-										<EstudiosCard universidad={estudios.Universidad} titulo={estudios.Titulo} lugar={estudios.lugar} fecha={estudios.fecha} logo={estudios.ImagenLogo} />
-									</div>
-								)
-							})
-						}
-					</div>
-				</div>
-
-
-			</div>
-
-			<div id="habilidades" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
-				<div className="fs-3 fw-bold mb-3">
-					Habilidades
-				</div>
-
-				<div className="d-flex">
-					<div className="row g-0 justify-content-center">
-						{
-							habilidades.map((habilidades, index) => {
-								return (
-									<div className="col-12 col-md-3 col-lg-2 m-0 p-0">
-										<HabilidadesCard habilidad={habilidades.habilidad} logo={habilidades.logo} />
-									</div>
-								)
-
-							})
-						}
+				<div id="educacion" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
+					<div className="fs-3 fw-bold mb-3">
+						Formación Académica
 					</div>
 
-				</div>
-
-			</div>
-
-
-
-			<div id="Certificaciones" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
-				<div id="certificaciones" className="fs-3 fw-bold mb-5 pb-2">
-					Certificaciones
-				</div>
-
-				<div className="d-flex">
-					<div className="row g-2 justify-content-center">
-						{
-							certificaciones.map((certificado, index) => {
-								return (
-									<div className="col-12 col-md-3 col-lg-5 m-1">
-										<CertificacionesCard entidad={certificado.entidad} titulo={certificado.Titulo} fecha={certificado.fecha} logo={certificado.logo} />
-									</div>
-								)
-							})
-						}
+					<div className="d-flex">
+						<div className="row g-0 justify-content-center">
+							{
+								estudiosFormales.map((estudios, index) => {
+									return (
+										<div className="col-md-5 m-1 p-2">
+											<EstudiosCard universidad={estudios.Universidad} titulo={estudios.Titulo} lugar={estudios.lugar} fecha={estudios.fecha} logo={estudios.ImagenLogo} />
+										</div>
+									)
+								})
+							}
+						</div>
 					</div>
+
+
+				</div>
+
+				<div id="habilidades" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
+					<div className="fs-3 fw-bold mb-3">
+						Habilidades
+					</div>
+
+					<div className="d-flex">
+						<div className="row g-0 justify-content-center">
+							{
+								habilidades.map((habilidades, index) => {
+									return (
+										<div className="col-12 col-md-3 col-lg-2 m-0 p-0">
+											<HabilidadesCard habilidad={habilidades.habilidad} logo={habilidades.logo} />
+										</div>
+									)
+
+								})
+							}
+						</div>
+
+					</div>
+
 				</div>
 
 
 
-			</div>
+				<div id="Certificaciones" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
+					<div id="certificaciones" className="fs-3 fw-bold mb-5 pb-2">
+						Certificaciones
+					</div>
+
+					<div className="d-flex">
+						<div className="row g-2 justify-content-center">
+							{
+								certificaciones.map((certificado, index) => {
+									return (
+										<div className="col-12 col-md-3 col-lg-5 m-1">
+											<CertificacionesCard entidad={certificado.entidad} titulo={certificado.Titulo} fecha={certificado.fecha} logo={certificado.logo} />
+										</div>
+									)
+								})
+							}
+						</div>
+					</div>
 
 
-			<div id="experiencia" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
-				<div id="experiencia" className="fs-3 fw-bold mb-5 pb-2">
-					Experiencia laboral
+
 				</div>
 
-			// Contenedor principal de la línea de tiempo
-				<div className="container py-5">
 
-					{/* La clase 'timeline-area' aplicará el CSS para la línea vertical */}
-					<div className="row timeline-area">
+				<div id="experiencia" className="justify-content-center mx-5 flex-column border-bottom border-custom-light-blue border-3 p-5">
+					<div id="experiencia" className="fs-3 fw-bold mb-2 pb-2">
+						Experiencia laboral
+					</div>
 
-						{/* =======================================================
+
+					<div className="container py-5">
+
+						{/* La clase 'timeline-area' aplicará el CSS para la línea vertical */}
+						<div className="row timeline-area">
+
+							{/* =======================================================
         Punto 1: Evento a la Izquierda 
         ======================================================= */}
-						<div className="col-12 col-md-6 timeline-left">
-							<div className="timeline-card timeline-card-right">
-								<h3 className="timeline-title">Título del Evento Izquierdo</h3>
-								<p className="timeline-date">Enero 2020</p>
-								<p>Descripción detallada del evento que ocurrió en la parte izquierda de la línea de tiempo.</p>
+							<div className="col-12 col-md-6 timeline-left">
+								<div className="timeline-card timeline-card-right">
+									<h3 className="timeline-title">Jefe de Operaciones</h3>
+									<h5 className="timeline-title">Estilo Ingenieria</h5>
+									<p className="timeline-date">Enero 2023 - Actual</p>
+									<p className="text-start">
+										<ul>
+											<li>
+												Optimicé procesos operativos gestionando métricas clave como tasa de falla, márgenes de ganancia, OTIF y NPS, aplicando
+												análisis de datos para identificar patrones y proponer mejoras basadas en evidencia.
+											</li>
+											<li>
+												Utilicé herramientas CRM como Salesforce y Dynamics para la gestión y seguimiento de clientes y operaciones, mejorando la
+												trazabilidad y reduciendo tiempos de respuesta.
+											</li>
+											<li>
+												Lideré equipos multidisciplinarios en la coordinación de proyectos de mantenimiento, aplicando metodologías de gestión ágil para
+												garantizar el cumplimiento de plazos y la satisfacción del cliente.
+											</li>
+										</ul>
+									</p>
+								</div>
 							</div>
-						</div>
 
-						{/* Columna vacía que actúa como separador en móviles */}
-						<div className="col-md-6 d-none d-md-block"></div>
+							{/* Columna vacía que actúa como separador en móviles */}
+							<div className="col-md-6 d-none d-md-block"></div>
 
 
-						{/* =======================================================
+							{/* =======================================================
         Punto 2: Evento a la Derecha 
         ======================================================= */}
-						<div className="col-md-6 d-none d-md-block"></div> {/* Columna vacía */}
+							<div className="col-md-6 d-none d-md-block"></div> {/* Columna vacía */}
 
-						<div className="col-12 col-md-6 timeline-right">
-							<div className="timeline-card timeline-card-left">
-								<h3 className="timeline-title">Título del Evento Derecho</h3>
-								<p className="timeline-date">Marzo 2021</p>
-								<p>Descripción detallada del evento que ocurrió en la parte derecha de la línea de tiempo.</p>
+							<div className="col-12 col-md-6 timeline-right">
+								<div className="timeline-card timeline-card-left text-start">
+									<h3 className="timeline-title ">Ingeneiro de Proyectos</h3>
+									<h5 className="timeline-title">Aeronautica Civil de Colombia</h5>
+									<p className="timeline-date">Septiembre de 2018 - Octubre de 2022</p>
+									<p>
+										<ul>
+											<li>
+												Lideré la formulación y estructuración de proyectos de infraestructura y tecnología aeronáutica, aplicando metodologías de
+												gestión y análisis de requisitos para garantizar la alineación técnica y operativa con los objetivos estratégicos.
+											</li>
+											<li>
+												Ejecuté evaluaciones técnicas, financieras y de factibilidad, optimizando la priorización de inversiones y fortaleciendo la toma de
+												decisiones basadas en datos y métricas de rendimiento.
+											</li>
+											<li>
+												Gestioné la coordinación con múltiples áreas técnicas y administrativas, asegurando la integración de recursos, el cumplimiento de
+												cronogramas y la mitigación de riesgos en todas las etapas del ciclo de vida del proyecto.
+											</li>
+										</ul>
+									</p>
+								</div>
 							</div>
-						</div>
 
-						{/* Repite esta estructura para cada evento (Izquierda/Derecha) */}
-				
-				
-									{/* =======================================================
+							{/* Repite esta estructura para cada evento (Izquierda/Derecha) */}
+
+
+							{/* =======================================================
         Punto 3: Evento a la Izquierda 
         ======================================================= */}
-						<div className="col-12 col-md-6 timeline-left">
-							<div className="timeline-card timeline-card-right">
-								<h3 className="timeline-title">Título del Evento Izquierdo</h3>
-								<p className="timeline-date">Enero 2020</p>
-								<p>Descripción detallada del evento que ocurrió en la parte izquierda de la línea de tiempo.</p>
+							<div className="col-12 col-md-6 timeline-left">
+								<div className="timeline-card timeline-card-right">
+									<h3 className="timeline-title">Superintendente Técnico</h3>
+									<h5 className="timeline-title">Otis Elevator Company Colombia SAS</h5>
+									<p className="timeline-date">Octubre de 2007 - Septiembre de 2018</p>
+									<p className="text-start">
+										<ul>
+											<li>
+												Supervisé y coordiné el mantenimiento de equipos críticos, asegurando alta disponibilidad y cumplimiento de estándares técnicos.
+											</li>
+											<li>
+												Optimicé procesos operativos mediante el uso de herramientas tecnológicas, incrementando la eficiencia y el control de tareas.
+											</li>
+											<li>
+												Analicé fallas recurrentes e implementé acciones correctivas que mejoraron la confiabilidad y redujeron incidentes.
+											</li>
+										</ul>
+									</p>
+								</div>
 							</div>
+
+							{/* Columna vacía que actúa como separador en móviles */}
+							<div className="col-md-6 d-none d-md-block"></div>
+
+
+
+
 						</div>
-
-						{/* Columna vacía que actúa como separador en móviles */}
-						<div className="col-md-6 d-none d-md-block"></div>
-
-
-						
-
 					</div>
+
+
+
 				</div>
 
-
-
 			</div>
-
-		</div>
 		</div>
 	);
 }; 
