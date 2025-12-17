81255da24e76fb63dc359462eddd3fbdfaaa0a83
Julian Andres Ramirez <116443557+jramirez0982@users.noreply.github.com>
2025-12-17 17:48:14 +0000

    [se mejora el footer para que sea responsive]

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -2,41 +2,48 @@ import logoB from "../assets/img/julian-ramirez.png";
 
 export const Footer = () => (
 
-	<footer style={{ backgroundColor: "#003366" }} className="text-white py-3">
-		<div className="container">
-			<div className="row text-center text-md-start align-items-center">
+	<footer style={{ backgroundColor: "#003366" }} className="text-white py-4">
+  <div className="container">
+    {/* Añadimos g-4 para dar espacio (gutter) entre las columnas cuando se apilen */}
+    <div className="row text-center text-lg-start align-items-center g-4">
 
+      {/* Columna 1: Logo */}
+      <div className="col-12 col-sm-6 col-lg-3">
+        <div className="d-flex justify-content-center justify-content-lg-start">
+          <img className="rounded-circle" src={logoB} alt="Logo" style={{ maxWidth: "100%", height: "auto" }} width={200} />
+        </div>
+      </div>
 
-				<div className="col-md-3 mb-2 mb-md-0">
-					<h5 className="mb-0 d-flex align-items-center">
-						<img className="rounded-circle" src={logoB} alt="Logo" width={200} />
-					</h5>
-				</div>
+      {/* Columna 2: Teléfono/Email */}
+      <div className="col-12 col-sm-6 col-lg-3">
+        <h6 className="fw-bold"><i className="fa-solid fa-phone"></i> Teléfono: +57 3113770557</h6>
+        <p className="mb-0"><strong><i className="fa-solid fa-envelope"></i> Email</strong></p>
+        <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href="mailto:julian.ramirez2507@gmail.com">
+          julian.ramirez2507@gmail.com
+        </a>
+      </div>
 
+      {/* Columna 3: Github */}
+      <div className="col-12 col-sm-6 col-lg-3">
+        <h6 className="fw-bold">Github</h6>
+        <p className="mb-0"><strong><i className="fa-brands fa-github"></i> Perfil</strong></p>
+        <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href="https://github.com/jramirez0982">
+          jramirez0982/
+        </a>
+      </div>
 
-				<div className="col-md-3 mb-2 mb-md-0">
-					<h6 className="fw-bold"><i class="fa-solid fa-phone"></i>  Teléfono: +57 3113770557</h6>
-					<p className="d-flex mb-0 justify-content center"><strong><i className="fa-solid fa-envelope"></i> Email</strong></p>
-					<a className="d-flex justify-content start" target="blank" href="mailto: julian.ramirez2507@gmail.com">julian.ramirez2507@gmail.com</a>
-				</div>
+      {/* Columna 4: Redes */}
+      <div className="col-12 col-sm-6 col-lg-3">
+        <h6 className="fw-bold">Redes</h6>
+        <p className="mb-0"><strong><i className="fa-brands fa-linkedin"></i> LinkedIn</strong></p>
+        <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/julian-andres-ramirez-ramirez/">
+          /in/julian-andres-ramirez-ramirez/
+        </a>
+      </div>
 
-				<div className="col-md-3 mb-2 mb-md-0">
-					<h6 className="fw-bold">Github</h6>
-					<p className="d-flex mb-0 justify-content center"><strong><i className="fa-brands fa-github"></i> Perfil de Github</strong></p>
-					<a className="d-flex justify-content start" target="blank" href="https://github.com/jramirez0982">jramirez0982/</a>
-
-				</div>
-
-
-				<div className="col-md-3">
-					<h6 className="fw-bold">Redes</h6>
-					<p className="d-flex mb-0 justify-content center"><strong><i class="fa-brands fa-linkedin"></i> Perfil de Linkedin</strong></p>
-				<a className="d-flex justify-content start" target="blank" href="https://www.linkedin.com/in/julian-andres-ramirez-ramirez/">/in/julian-andres-ramirez-ramirez/</a>
-
-				</div>
-			</div>
-		</div>
-	</footer>
+    </div>
+  </div>
+</footer>
 );
 
 
